67b0351d14c72c164236246d77a1f85e2a46381b
kopteryx <167141197+kopteryx@users.noreply.github.com>
2024-07-19 19:05:53 -0500

    Add files via upload
    Radha helped code to log the data to a .csv file: Note that that the data appends to the end of each file after each run.

--- Arduino_Nano_BLE_Test/Nano33_BLE_Rev2_Main/Nano33_BLE_Rev2_Main.ino
+++ Arduino_Nano_BLE_Test/Nano33_BLE_Rev2_Main/Nano33_BLE_Rev2_Main.ino
@@ -13,6 +13,7 @@
 #include <Wire.h>
 #include "SparkFun_Qwiic_OpenLog_Arduino_Library.h"
 #define RELAY_PIN  2  // The Arduino Nano pin connected to the IN pin of relay module
+#include <string.h>
 
 OpenLog myLog;
 RTC_DS3231 rtc;
@@ -23,21 +24,22 @@ private:
     bool reached_desired_altitude; //Boolean to determine whether the desired altitude was reached or not
 public:
     OneWaySwitch() : reached_desired_altitude(false) {}
-    void turnOn() {
+    bool turnOn() {
         reached_desired_altitude = true;
         
         for (int i = 0; i < 2; i++) {
           digitalWrite(RELAY_PIN, HIGH);
-          Serial.println("Relay is ON");
-          myLog.println("Relay is ON");
+          //Serial.println("Relay is ON");
+          // myLog.println("Relay is ON");
           delay(12000);
           digitalWrite(RELAY_PIN, LOW);
-          Serial.println("Relay is OFF");
-          myLog.println("Relay is OFF");
+          //Serial.println("Relay is OFF");
+          // myLog.println("Relay is OFF");
           delay(10000);
         }
 
-        myLog.println();
+        // myLog.println();
+        return true;
     }
     bool getState() const {
         return reached_desired_altitude;
@@ -45,14 +47,17 @@ public:
 };
 
 OneWaySwitch switch1;    // CHANGE TO CORRECT ALTITUDE + SEA LEVEL BEFORE FLIGHT
-float desired_altitude = 282.5; //Desired altitude in meters
+float desired_altitude = 100; //Desired altitude in meters: 80,000 ft = 24720
+bool isCut;
+String data = "";
 
 void setup() {
   Wire.begin();
   myLog.begin();
+  isCut = false;
   
-  Serial.begin(9600);
-  while (!Serial);
+  //Serial.begin(9600);
+  //while (!Serial);
 
   if (!BARO.begin()) {
     Serial.println("Failed to initialize pressure sensor!");
@@ -66,56 +71,85 @@ void setup() {
 
   rtc.begin();
   rtc.adjust(DateTime(F(__DATE__),F(__TIME__)));
-  //rtc.adjust(DateTime(2024, 7, 15, 11, 34, 0));
+  //rtc.adjust(DateTime(2024, 7, 19, 17, 56, 0));
 
   // initialize digital pin as an output.
   pinMode(RELAY_PIN, OUTPUT);
 
-  myLog.append("Nano33_BLE_Rev2_Data.txt");
+  String fileName = "Nano33_BLE_Rev2_Data.csv";
+
+  myLog.create(fileName);
+  myLog.append(fileName);
   myLog.syncFile();
   
-  myLog.println();
-  myLog.println();
-  myLog.println();
+  // myLog.println();
+  // myLog.println();
+  myLog.print("Started Logging New Data!\n");
+  myLog.print("Date/Time,Pressure(kPa),Temperature(C),Humidity(%), Altitude(m),Relay(on/off)\n");
   //Serial.println("SD is sucessufully logging!");
-  myLog.println("Started Logging New Data!");
-  myLog.println();
+  // myLog.println();
 }
 
 void loop() {
     DateTime now = rtc.now();
     sprintf(t, "%02d:%02d:%02d %02d/%02d/%02d", now.hour(), now.minute(), now.second(), now.day(), now.month(), now.year());  
-    myLog.print(F("Date/Time: "));
-    myLog.println(t);
+    // myLog.print(F("Date/Time: "));
+    // myLog.println(t);
+    data += t;
+    data += ",";
 
     float pressure = BARO.readPressure();
 
-    myLog.print("Pressure    = ");
-    myLog.print(pressure);
-    myLog.println(" kPa");
+    // myLog.print("Pressure    = ");
+    // myLog.print(pressure);
+    // myLog.println(" kPa");
+    data += String(pressure);
+    data += ",";
 
     float temperature = BARO.readTemperature();
     float humidity    = HS300x.readHumidity();
 
-    myLog.print("Humidity    = ");
-    myLog.print(humidity);
-    myLog.println(" %");
+    // myLog.print("Humidity    = ");
+    // myLog.print(humidity);
+    // myLog.println(" %");
+    data += String(humidity);
+    data += ",";
 
-    myLog.print("Temperature = ");
-    myLog.print(temperature);
-    myLog.println(" C");
+    // myLog.print("Temperature = ");
+    // myLog.print(temperature);
+    // myLog.println(" C");
+    data += String(temperature);
+    data += ",";
 
     float altitude = 44330 * ( 1 - pow(pressure/101.325, 1/5.255) );
     
-    myLog.print("Altitude according to kPa = ");
-    myLog.print(altitude);
-    myLog.println(" m");
-    myLog.println();
+    // myLog.print("Altitude according to kPa = ");
+    // myLog.print(altitude);
+    // myLog.println(" m");
+    // myLog.println();
+    data += String(altitude);
+    data += ",";
+
+    /*
+    Serial.print("Altitude according to kPa = ");
+    Serial.print(altitude);
+    Serial.println(" m");
+    Serial.println();
+    */
 
     //Checks if current altitude has reached the desired altitiude, then turns on a one way switch to active the release mechanism
     if (altitude >= desired_altitude && !switch1.getState()) {
-        switch1.turnOn();
+        isCut = switch1.turnOn();
     }
+
+    data += String(isCut);
+    data += ",";
+    data += "\n";
+
+    myLog.print(data);
+
+    data = "";
+    isCut = false;
 
     // wait 1 second to print again
     delay(1000);
